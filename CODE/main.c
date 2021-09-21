b4e134b995aee64eb25bddaeac51652f168b7425
Mathilde Bonin <nanam@sfr.fr>
2021-09-21 11:10:53 +0200

    Création de DBParams

--- CODE/main.c
+++ CODE/main.c
@@ -2,8 +2,19 @@
 
 #include <stdio.h>
 
+typedef struct {
+    char* DBPath;//4096 = taille max d'un path linux
+    unsigned int pageSize;
+    char maxPagesPerFile;
+}DBParams;
 
-int main(int argc, char *argv[]) {
+DBParams params;
+
+int main(int argc, char **argv) {
+    params.DBPath = argv[1]; 
+    params.pageSize = 4096;
+    params.maxPagesPerFile = 4;
+
     printf("Bonjour :)\n");
     return 0;
 }
