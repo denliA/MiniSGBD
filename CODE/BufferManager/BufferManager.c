12a0ec0a83aba367d0cd0b149de1bdbca73d49f9
Deniza <999.denli@gmail.com>
2021-09-28 12:41:44 +0200

    Implementation de FreePage

--- CODE/BufferManager/BufferManager.c
+++ CODE/BufferManager/BufferManager.c
@@ -8,6 +8,30 @@
 //variable globale de buffer pool
 
 uint8_t *GetPage(PageId pageId){
+
+}
+
+void FreePage(PageId pageId, int valdirty){
+	count++;
+	int i;
+	//recherche de la bonne case dans le tableau frames
+	for (i=0;i<nframes;i++){
+		if (frames[i].pageId == pageId){
+			break;
+		}
+	}
+	if (i==nframes){ //page pas trouvée
+		fprintf(stderr, "Page de id %d pas trouvée",pageId);
+		return;
+	}
+	frames[i].pin_count--;
+	if (frames[i].pin_count==0)
+		frames[i].lastUnpin=count;
+	//attention si l'ancienne valeur de dirty vaut 1, elle reste à 1
+	if (frames[i].dirty==0)
+		frames[i].dirty=valdirty;
+
+}
     
 
 
