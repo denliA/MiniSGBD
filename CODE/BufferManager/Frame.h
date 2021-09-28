00305d01d43e04dd979e0b7c584826063c2ea075
Mathilde Bonin <nanam@sfr.fr>
2021-09-28 11:52:58 +0200

    BufferManager créé

--- CODE/BufferManager/Frame.h
+++ CODE/BufferManager/Frame.h
@@ -1,10 +1,12 @@
 #ifndef FRAME_H
 #define FRAME_H
 
-#include <PageId.h>
+#include "PageId.h"
 
 typedef struct _Frame{
+    //adresse du buffer contenant la page dans le BufferPool
     uint8_t *buffer;
+    //Id de la page voulue dans la DB 
     PageId pageId;
     int pin_count;
     unsigned dirty : 1;
