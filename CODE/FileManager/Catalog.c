b23d9e356121474986c650867deaac6999d59b55
Deniza <999.denli@gmail.com>
2021-10-05 11:06:57 +0200

    Catalog.c et Catalog.h

--- CODE/FileManager/Catalog.c
+++ CODE/FileManager/Catalog.c
@@ -5,4 +5,29 @@
  *      Author: denli
  */
 
+#include <stdio.h>
+#include <sdtlib.h>
+#include <RelationInfo.h>
+#define CAT_SIZE 50;
+
+typedef struct _Catalog{
+	RelationInfo** tab;
+	int cpt;
+}Catalog;
+
+void Init(Catalog* cat){
+	cat->tab=(RelationInfo**)calloc(CAT_SIZE,sizeof(*RelationInfo));
+	cat->cpt=0;
+}
+
+void Finish(Catalog* cat){
+	free(cat->tab);
+	free(cat);
+}
+
+void AddRelation(RelationInfo * rel,Catalog* cat){
+	//
+	cat->tab[cat->cpt] = rel;
+	cat->cpt++;
+}
 
