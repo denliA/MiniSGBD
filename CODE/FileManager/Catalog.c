953ac50c8065532ecc046e1cec7d7adebd8ee47f
Deniza <999.denli@gmail.com>
2021-10-05 11:42:30 +0200

    Catalog.c

--- CODE/FileManager/Catalog.c
+++ CODE/FileManager/Catalog.c
@@ -6,18 +6,20 @@
  */
 
 #include <stdio.h>
-#include <sdtlib.h>
+#include <stdlib.h>
 #include <RelationInfo.h>
-#define CAT_SIZE 50;
+#define CAT_SIZE 100
 
 typedef struct _Catalog{
-	RelationInfo** tab;
+	RelationInfo* tab;
 	int cpt;
+	int sizeMax;
 }Catalog;
 
 void Init(Catalog* cat){
-	cat->tab=(RelationInfo**)calloc(CAT_SIZE,sizeof(*RelationInfo));
+	cat->tab=(RelationInfo*)calloc(CAT_SIZE,sizeof(RelationInfo));
 	cat->cpt=0;
+	cat->sizeMax=CAT_SIZE; //la taille max du tableau de catalog
 }
 
 void Finish(Catalog* cat){
@@ -26,7 +28,16 @@ void Finish(Catalog* cat){
 }
 
 void AddRelation(RelationInfo * rel,Catalog* cat){
-	//
+	//realloc si le tableau est rempli
+	if ((cat->cpt)>cat->sizeMax){
+		//si realloc echoue, il retourne NULL
+		RelationInfo* p = realloc(cat->tab,cat->sizeMax*2);
+		if (p == NULL){
+			fprintf( stderr, "Echec de réallocation de cat->tab");
+		}
+		cat->tab = p;
+		cat->sizeMax=(cat->sizeMax)*2;
+	}
 	cat->tab[cat->cpt] = rel;
 	cat->cpt++;
 }
