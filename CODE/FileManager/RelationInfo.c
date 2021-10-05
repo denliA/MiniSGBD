e63596c9d19582e3e741ac9364bb27a8f65fdb84
Axel <ppd491@protonmail.com>
2021-10-05 11:04:12 +0200

    modified:   CODE/FileManager/RelationInfo.c 	modified:   Makefile

--- CODE/FileManager/RelationInfo.c
+++ CODE/FileManager/RelationInfo.c
@@ -8,12 +8,19 @@
 void RelationInfoInit(RelationInfo* rel){
 	rel->name=(char*)calloc(CHAR_SIZE,sizeof(char));
 	rel->colNames=(char**)calloc(COLUMN_AMOUNT,sizeof(char*));
-	rel->colType=(char**)calloc(COLUMN_AMOUNT,sizeof(char*));
+	rel->colType=(char**)calloc(COLUMN_AMOUNT,sizeof(int*));
 
 	for (int i=0;i<COLUMN_AMOUNT;i++){
 		rel->colNames[i]=(char*)calloc(CHAR_SIZE,sizeof(char));
-		rel->colType[i]=(char*)calloc(CHAR_SIZE,sizeof(char));
+		rel->colType[i]=(int*)calloc(1,sizeof(int));
 	}
 
+}
 
+void RelationInfoFinish(RelationInfo* rel){
+	for (int i=0;i< COLUMN_AMOUNT;i++){
+		free(rel->colNames[i]);
+		free(rel->colType[i]);
+	}
+	free(rel->name);
 }
