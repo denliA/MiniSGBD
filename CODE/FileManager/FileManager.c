a6d152a6be6df947bf18377f510a911ae2984590
Mathilde Bonin <nanam@sfr.fr>
2021-10-12 12:40:05 +0200

    ajout de la fonction createHeaderPage

--- CODE/FileManager/FileManager.c
+++ CODE/FileManager/FileManager.c
@@ -18,3 +18,22 @@ PageId readPageIdFromPageBuffer(uint8_t *buff, uint8_t first){
 	return *ptr;
 }
 
+
+PageId createHeaderPage(){
+    //allocation de la page
+    PageId pageId = AllocPage();
+    
+    //écriture sur la page
+    uint8_t *headerPage = GetPage(pageId);
+    //création du pageId factice valant (-1,0)
+    PageId factice;
+    factice.FileIdx = -1;
+    factice.PageIdx = 0;
+    //écriture de ce pageId factice deux fois dans la page (1er et 2e pageID)
+    writePageIdToPageBuffer(factice,headerPage,0);
+    writePageIdToPageBuffer(factice,headerPage,1);
+    //on libère la page qui a été modifiée donc dirty à 1
+    FreePage(pageId,1);
+    return pageId;
+}
+
