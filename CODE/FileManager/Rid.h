f824006ea6bee6b335957d2ed0851d2eca022f05
Mathilde Bonin <nanam@sfr.fr>
2021-10-12 11:39:31 +0200

    Création et complétude de RID

---
+++ CODE/FileManager/Rid.h
@@ -0,0 +1,14 @@
+#ifndef RID_H
+#define RID_H
+
+#include <stdint.h>
+#include "../DiskManager/PageId.h"
+
+#define equalRid(rid1,rid2) (equalPageId((rid1).pageId, (rid2).pageId))&&((rid1).slotIdx==(rid2).slotIdx))
+
+typedef struct _rid{
+    PageId pageId;
+    uint32_t slotIdx;
+}Rid;
+
+#endif
