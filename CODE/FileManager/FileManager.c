ceed3ef729beaa80c5324d9a64884de1ad83e574
Axel <ppd491@protonmail.com>
2021-10-12 12:37:33 +0200

    modified:   CODE/FileManager/FileManager.c

--- CODE/FileManager/FileManager.c
+++ CODE/FileManager/FileManager.c
@@ -1,8 +1,16 @@
-/*
- * FileManager.c
- *
- *  Created on: 12 oct. 2021
- *      Author: ij07411
- */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../DiskManager/PageId.h"
+
+#ifndef PAGEID_SIZE
+#define PAGEID_SIZE 5
 
 
+void writePageIdToPageBuffer(PageId pageId, uint8_t* buff, int first){
+	PageId *deb=(PageId*)&buff[PAGEID_SIZE*first];
+	*deb=pageId;
+
+}
+
+#endif
