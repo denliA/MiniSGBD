57b2cdb907c984e40bb4277fee43bcdac4be6afe
Djamel Ait-Moussa <d.w.aitmoussa@gmail.com>
2021-11-09 08:46:34 +0100

    ajout squelette de ProcessCommand

--- CODE/DBManager/DBManager.c
+++ CODE/DBManager/DBManager.c
@@ -1,7 +1,11 @@
+#include <string.h>
+
 #include "DBManager.h"
 #include "BufferManager/BufferManager.h"
 
 extern  Catalog cat;
+
+#define commeq(big, little) ( strncmp( (big), (little), strlen(little) ) == 0 ) 
 
 void InitDBM(void){
 	InitCatalog();
@@ -12,7 +16,19 @@ void FinishDBM(void){
 	FlushAll();
 }
 
-//affiche
-void ProcessCommand(char* command){
 
+
+void ProcessCommand(char* command){
+    if(commeq(command, "CREATE RELATION")) {
+        // 
+    } else if (commeq(command, "DROPDB")) {
+    
+    } else if (commeq(command, "INSERT")) {
+    
+    } else if (commeq(command, "BATCHINSERT")) {
+    
+    } else if (commeq(command, "SELECTMONO")) {
+    
+    } else 
+        printf("Erreur: commande \"%S\" inconnue\n", command);
 }
