314b6ab6299508a7ecb5a520712f3b6a8d37ca88
Djamel Wassim Ait Moussa <d.w.aitmoussa@gmail.com>
2021-12-07 12:48:54 +0100

    Ajout signatures des itérateurs sur les pages

--- CODE/FileManager/FileManager.h
+++ CODE/FileManager/FileManager.h
@@ -19,12 +19,51 @@ typedef struct _ListRecordsIterator {
         iter->currentSlot = _currentSlot;\
     } \
 
-PageId createHeaderPage(void);
 
 ListRecordsIterator *GetListRecordsIterator(RelationInfo *rel);
 Record *GetNextRecord(ListRecordsIterator *iterator);
 
 
+typedef struct _PageIterator {
+    RelationInfo *rel;
+    int8_t currentList;
+    PageId currentPage;
+    uint8_t *pageBuffer
+} PageIterator;
+
+typedef struct _RecordsOnPageIterator {
+    RelationInfo *rel;
+    uint8_t *pageBuffer;
+    int32_t currentSlot;
+} RecordsOnPageIterator;
+
+PageIterator *GetPageIterator(RelationInfo *rel); // Crée un itérateur sur les pages de la relation rel
+uint8_t *GetNextPage(PageIterator *iter); // Retourne le buffer de la prochaine page, et NULL si il n'y en a plus
+
+RecordsOnPageIterator *GetRecordsOnPageIterator(RelationInfo *rel, uint8_t buffer);
+Record *GetNextRecordOnPage(RecordsOnPageIterator *iter);
+
+/************UTILISATION DES ITERATEURS SUR LES RECORDS ET SUR LES PAGES ************
+Exemple : Si on a une relation "RelationInfo *rel" et on veut itérer sur chacune des pages,
+          et pour chaque page on itère sur chacun des records et on les print
+
+PageIterator *pageiter = GetPageIterator(rel); // On initialise l'itérateur
+RecordsOnPageIterator *recorditer; // On prépare un itérateur de records sur une page
+uint8_t *buffer_de_page = GetNextPage(pageiter); // GetNextPage nous retourne le buffer de la prochaine page disponible
+while (buffer_de_page != NULL) {
+    recorditer = GetRecordsOnPageIterator(rel, buffer_de_page); // On initialise l'itérateur des records
+    Record *record = GetNextRecord(recorditer); // retourne le prochian record dispo
+    while( record != NULL) {
+        printRecord(record); // Ici on affiche juste le record. 
+        record = GetNextRecord(recorditer);
+    }
+    GetNextPage(pageiter);
+}
+
+/************************************************************************************/
+
+
+PageId createHeaderPage(void);
 Rid InsertRecordIntoRelation(RelationInfo *rel, Record *rec);
 void DeleteRecordFromRelation(RelationInfo *rel, Rid rid);
 void UpdateRecord(Record *record);
